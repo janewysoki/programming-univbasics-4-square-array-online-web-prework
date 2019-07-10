6c9935adbb855a9ba56e18ecaf0d87757c3fd333
Jane Wysoki <jane.wysoki@gmail.com>
2019-07-10 20:18:51 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,9 +2,7 @@ def square_array(array)
   squared_numbers = []
   counter = 0
   while counter < array.length do
-    squared_numbers << array[counter]
-  
-    
+    squared_numbers << (array[counter]**2)
     counter += 1
   end
   squared_numbers
